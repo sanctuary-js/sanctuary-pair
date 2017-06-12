82ad22174f8c7d2324cfe09772685f152e802ab6
Gabe Johnson <gijohnson105@gmail.com>
2017-06-11 22:14:44 -0500

    Remove Identity from the package

--- index.js
+++ index.js
@@ -195,10 +195,6 @@
     return Z.lte(this.a, other.a) && Z.lte(this.b, other.b);
   };
 
-  function makePair(p, a, b) {
-    return p instanceof Identity ? Identity(b) : Pair(a, b);
-  }
-
   //# Pair#fantasy-land/compose :: Pair a b ~> Pair b c -> Pair a c
   //.
   //. ```javascript
@@ -206,7 +202,7 @@
   //. Pair(1, true)
   //. ```
   Pair.prototype['fantasy-land/compose'] = function compose(other) {
-    return makePair(this, this.a, other.b);
+    return Pair(this.a, other.b);
   };
 
   //# Pair#fantasy-land/concat :: (Semigroup a, Semigroup b) => Pair a b ~> Pair a b -> Pair a b
@@ -216,8 +212,7 @@
   //. Pair([1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1])
   //. ```
   Pair.prototype['fantasy-land/concat'] = function concat(other) {
-    return makePair(this,
-                    Z.concat(this.a, other.a), Z.concat(this.b, other.b));
+    return Pair(Z.concat(this.a, other.a), Z.concat(this.b, other.b));
   };
 
   //# Pair#fantasy-land/map :: Pair a b ~> (b -> c) -> Pair a c
@@ -227,7 +222,7 @@
   //. Pair('hello', 8)
   //. ```
   Pair.prototype['fantasy-land/map'] = function map(f) {
-    return makePair(this, this.a, f(this.b));
+    return Pair(this.a, f(this.b));
   };
 
   //# Pair#fantasy-land/ap :: Semigroup a => Pair a b ~> Pair a (b -> c) -> Pair a c
@@ -237,7 +232,7 @@
   //. Pair('hello there', 8)
   //. ```
   Pair.prototype['fantasy-land/ap'] = function ap(other) {
-    return makePair(this, Z.concat(this.a, other.a), other.b(this.b));
+    return Pair(Z.concat(this.a, other.a), other.b(this.b));
   };
 
   //# Pair#fantasy-land/chain :: Semigroup a => Pair a b ~> (b -> Pair a c) -> Pair a c
@@ -270,7 +265,7 @@
   Pair.prototype['fantasy-land/traverse'] = function traverse(typeRep, f) {
     var pair = this;
     return Z.map(function(x) {
-      return makePair(pair, pair.a, x);
+      return Pair(pair.a, x);
     }, f(pair.b));
   };
 
@@ -281,7 +276,7 @@
   //. Pair('forever', 100)
   //. ```
   Pair.prototype['fantasy-land/extend'] = function extend(f) {
-    return makePair(this, this.a, f(this));
+    return Pair(this.a, f(this));
   };
 
   //# Pair#fantasy-land/extract :: Pair a b ~> () -> b
@@ -322,163 +317,12 @@
   //. ```
   var snd = Z.extract;
 
-  //. Identity (1-tuple) is the simplest container type: a value of type
-  //. `Identity a` always contains exactly one value, of type `a`.
-  //.
-  //. `Identity a` satisfies the following [Fantasy Land][] specifications:
-  //.
-  //.   - [Setoid][] (if `a` satisfies Setoid)
-  //.   - [Ord][] (if `a` satisfies Ord)
-  //.   - [Semigroup][] (if `a` satisfies Semigroup)
-  //.   - [Functor][]
-  //.   - [Apply][]
-  //.   - [Applicative][]
-  //.   - [Chain][]
-  //.   - [Monad][]
-  //.   - [Alt][] (if `a` satisfies Alt)
-  //.   - [Foldable][]
-  //.   - [Traversable][]
-  //.   - [Extend][]
-  //.   - [Extract][]
-
-  //# Identity :: a -> Identity a
-  //.
-  //. ```javascript
-  //. > Identity(42)
-  //. Identity(42)
-  //. ```
-  function Identity(value) {
-    if (!(this instanceof Identity)) return new Identity(value);
-    Pair.call(this, Unit, value);
-  }
-  Identity.prototype = Object.create(Pair.prototype);
-
-  //# Identity.@@type :: String
-  //.
-  //. ```javascript
-  //. > Identity['@@type']
-  //. 'sanctuary-tuples/Identity'
-  //. ```
-  Identity['@@type'] = 'sanctuary-tuples/Identity';
-
-  //# Identity.fantasy-land/of :: a -> Identity a
-  //.
-  //. ```javascript
-  //. > Z.of(Identity, 42)
-  //. Identity(42)
-  //. ```
-  Identity['fantasy-land/of'] = Identity;
-
-  //# Identity#fantasy-land/equals :: Setoid a => Identity a ~> Identity a -> Boolean
-  //.
-  //. ```javascript
-  //. > Z.equals(Identity([1, 2, 3]), Identity([1, 2, 3]))
-  //. true
-  //.
-  //. > Z.equals(Identity([1, 2, 3]), Identity([3, 2, 1]))
-  //. false
-  //. ```
-
-  //# Identity#fantasy-land/lte :: Ord a => Identity a ~> Identity a -> Boolean
-  //.
-  //. ```javascript
-  //. > Z.lte(Identity(0), Identity(0))
-  //. true
-  //.
-  //. > Z.lte(Identity(0), Identity(1))
-  //. true
-  //.
-  //. > Z.lte(Identity(1), Identity(0))
-  //. false
-  //. ```
-
-  //# Identity#fantasy-land/concat :: Semigroup a => Identity a ~> Identity a -> Identity a
-  //.
-  //. ```javascript
-  //. > Z.concat(Identity([1, 2, 3]), Identity([4, 5, 6]))
-  //. Identity([1, 2, 3, 4, 5, 6])
-  //. ```
-
-  //# Identity#fantasy-land/map :: Identity a ~> (a -> b) -> Identity b
-  //.
-  //. ```javascript
-  //. > Z.map(Math.sqrt, Identity(64))
-  //. Identity(8)
-  //. ```
-
-  //# Identity#fantasy-land/ap :: Identity a ~> Identity (a -> b) -> Identity b
-  //.
-  //. ```javascript
-  //. > Z.ap(Identity(Math.sqrt), Identity(64))
-  //. Identity(8)
-  //. ```
-
-  //# Identity#fantasy-land/chain :: Identity a ~> (a -> Identity b) -> Identity b
-  //.
-  //. ```javascript
-  //. > Z.chain(n => Identity(n + 1), Identity(99))
-  //. Identity(100)
-  //. ```
-  Identity.prototype['fantasy-land/chain'] = function(f) {
-    return f(this.b);
-  };
-
-  //# Identity#fantasy-land/alt :: Alt a => Identity a ~> Identity a -> Identity a
-  //.
-  //. ```javascript
-  //. > Z.alt(Identity([1, 2, 3]), Identity([4, 5, 6]))
-  //. Identity([1, 2, 3, 4, 5, 6])
-  //. ```
-  Identity.prototype['fantasy-land/alt'] = function(other) {
-    return Identity(Z.alt(this.b, other.b));
-  };
-
-  //# Identity#fantasy-land/reduce :: Identity a ~> ((b, a) -> b, b) -> b
-  //.
-  //. ```javascript
-  //. > Z.reduce(Z.concat, [1, 2, 3], Identity([4, 5, 6]))
-  //. [1, 2, 3, 4, 5, 6]
-  //. ```
-
-  //# Identity#fantasy-land/traverse :: Applicative f => Identity a ~> (TypeRep f, a -> f b) -> f (Identity b)
-  //.
-  //. ```javascript
-  //. > Z.traverse(Array, x => [x, x], Identity(0))
-  //. [Identity(0), Identity(0)]
-  //. ```
-
-  //# Identity#fantasy-land/extend :: Identity a ~> (Identity a -> b) -> Identity b
-  //.
-  //. ```javascript
-  //. > Z.extend(identity => Z.extract(identity) + 1, Identity(99))
-  //. Identity(100)
-  //. ```
-
-  //# Identity#fantasy-land/extract :: Identity a ~> () -> a
-  //.
-  //. ```javascript
-  //. > Z.extract(Identity(42))
-  //. 42
-  //. ```
-
-  //# Identity#toString :: Identity a ~> () -> String
-  //.
-  //. ```javascript
-  //. > Z.toString(Identity([1, 2, 3]))
-  //. 'Identity([1, 2, 3])'
-  //. ```
-  Identity.prototype.toString = function() {
-    return 'Identity(' + Z.toString(this.b) + ')';
-  };
-
   return {
-    Unit: Unit, Pair: Pair, Identity: Identity, fst: fst, snd: snd
+    Unit: Unit, Pair: Pair, fst: fst, snd: snd
   };
 
 }));
 
-//. [Alt]:              v:fantasyland/fantasy-land#alt
-//. [Applicative]:      v:fantasyland/fantasy-land#applicative
 //. [Apply]:            v:fantasyland/fantasy-land#apply
 //. [Chain]:            v:fantasyland/fantasy-land#chain
 //. [Extend]:           v:fantasyland/fantasy-land#extend
@@ -486,7 +330,6 @@
 //. [Fantasy Land]:     v:fantasyland/fantasy-land
 //. [Foldable]:         v:fantasyland/fantasy-land#foldable
 //. [Functor]:          v:fantasyland/fantasy-land#functor
-//. [Monad]:            v:fantasyland/fantasy-land#monad
 //. [Monoid]:           v:fantasyland/fantasy-land#monoid
 //. [Ord]:              v:fantasyland/fantasy-land#ord
 //. [Semigroup]:        v:fantasyland/fantasy-land#semigroup
--- test/index.js
+++ test/index.js
@@ -7,7 +7,6 @@ var Z = require('sanctuary-type-classes');
 var Tuples = require('..');
 var Unit = Tuples.Unit;
 var Pair = Tuples.Pair;
-var Identity = Tuples.Identity;
 var fst = Tuples.fst;
 var snd = Tuples.snd;
 
@@ -32,9 +31,6 @@ test('equals', function() {
   eq(Z.equals(Pair([1, 2, 3], [3, 2, 1]), Pair([1, 2, 3], [3, 2, 1])), true);
   eq(Z.equals(Z.equals(Pair([1, 2, 3], [3, 2, 1]),
                        Pair([1, 2, 3], [1, 2, 3]))), false);
-
-  eq(Z.equals(Identity([1, 2, 3]), Identity([1, 2, 3])), true);
-  eq(Z.equals(Identity([1, 2, 3]), Identity([3, 2, 1])), false);
 });
 
 test('lte', function() {
@@ -44,10 +40,6 @@ test('lte', function() {
   eq(Z.lte(Pair(0, 1), Pair(0, 1)), true);
   eq(Z.lte(Pair(0, 1), Pair(1, 1)), true);
   eq(Z.lte(Pair(1, 1), Pair(0, 1)), false);
-
-  eq(Z.lte(Identity(0), Identity(0)), true);
-  eq(Z.lte(Identity(0), Identity(1)), true);
-  eq(Z.lte(Identity(1), Identity(0)), false);
 });
 
 test('compose', function() {
@@ -59,31 +51,22 @@ test('concat', function() {
 
   eq(Z.concat(Pair([1, 2, 3], [6, 5, 4]), Pair([4, 5, 6], [3, 2, 1])),
      Pair([1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]));
+});
 
-  eq(Z.concat(Identity([1, 2]), Identity([3, 4])), Identity([1, 2, 3, 4]));
 test('empty', function() {
   eq(Z.empty(Unit.constructor), Unit);
 });
 
 test('map', function() {
   eq(Z.map(Math.sqrt, Pair('hello', 64)), Pair('hello', 8));
-
-  eq(Z.map(Math.sqrt, Identity(9)), Identity(3));
 });
 
 test('ap', function() {
   eq(Z.ap(Pair(' there', Math.sqrt), Pair('hello', 64)),
      Pair('hello there', 8));
-
-  eq(Z.ap(Identity(Math.sqrt), Identity(9)), Identity(3));
 });
 
 test('chain', function() {
-  eq(Z.chain(compose(Identity, Math.sqrt), Identity(9)), Identity(3));
-});
-
-test('alt', function() {
-  eq(Z.alt(Identity([1, 2]), Identity([3, 4])), Identity([1, 2, 3, 4]));
   eq(Z.chain(function(n) { return Pair([n], n + 1); },
              Pair([1], 2)), Pair([1, 2], 3));
 });
@@ -91,35 +74,24 @@ test('alt', function() {
 test('reduce', function() {
   eq(Z.reduce(Z.concat, [1, 2, 3], Pair('irrelevant', [4, 5, 6])),
      [1, 2, 3, 4, 5, 6]);
-
-  eq(Z.reduce(Z.concat, [1, 2], Identity([3, 4])), [1, 2, 3, 4]);
 });
 
 test('traverse', function() {
   eq(Z.traverse(Array, function(s) { return s.split(''); }, Pair(0, 'abc')),
      [Pair(0, 'a'), Pair(0, 'b'), Pair(0, 'c')]);
-
-  eq(Z.traverse(Array, function(s) { return s.split(''); }, Identity('abc')),
-     [Identity('a'), Identity('b'), Identity('c')]);
 });
 
 test('extend', function() {
   eq(Z.extend(function(pair) { return Z.extract(pair) + 1; },
               Pair('forever', 99)), Pair('forever', 100));
-
-  eq(Z.extend(compose(Math.sqrt, Z.extract), Identity(9)), Identity(3));
 });
 
 test('extract', function() {
   eq(Z.extract(Pair(0, 1)), 1);
-
-  eq(Z.extract(Identity(0)), 0);
 });
 
 test('toString', function() {
   eq(Z.toString(Pair([1, 2], 3)), 'Pair([1, 2], 3)');
-
-  eq(Z.toString(Identity([1, 2, 3])), 'Identity([1, 2, 3])');
 });
 
 test('fst', function() {
