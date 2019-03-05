26c56edc16983b87ba2583dde76d56b677c4a3a2
David Chambers <dc@davidchambers.me>
2019-03-05 19:40:17 +0100

    unify environment detection

--- index.js
+++ index.js
@@ -18,17 +18,24 @@
 
   'use strict';
 
+  var util = {inspect: {}};
+
   /* istanbul ignore else */
   if (typeof module === 'object' && typeof module.exports === 'object') {
-    module.exports = f (require ('sanctuary-show'),
+    module.exports = f (require ('util'),
+                        require ('sanctuary-show'),
                         require ('sanctuary-type-classes'));
   } else if (typeof define === 'function' && define.amd != null) {
-    define (['sanctuary-show', 'sanctuary-type-classes'], f);
+    define (['sanctuary-show', 'sanctuary-type-classes'], function(show, Z) {
+      return f (util, show, Z);
+    });
   } else {
-    self.sanctuaryPair = f (self.sanctuaryShow, self.sanctuaryTypeClasses);
+    self.sanctuaryPair = f (util,
+                            self.sanctuaryShow,
+                            self.sanctuaryTypeClasses);
   }
 
-} (function(show, Z) {
+} (function(util, show, Z) {
 
   'use strict';
 
@@ -64,15 +71,13 @@
     /* eslint-enable key-spacing */
   };
 
-  var util =
-    typeof module === 'object' && typeof module.exports === 'object' ?
-    require ('util') :
-    /* istanbul ignore next */ {};
-  prototype[
-    util.inspect != null && typeof util.inspect.custom === 'symbol' ?
-    /* istanbul ignore next */ util.inspect.custom :
-    /* istanbul ignore next */ 'inspect'
-  ] = Pair$prototype$show;
+  var custom = util.inspect.custom;
+  /* istanbul ignore else */
+  if (typeof custom === 'symbol') {
+    prototype[custom] = Pair$prototype$show;
+  } else {
+    prototype.inspect = Pair$prototype$show;
+  }
 
   /* istanbul ignore else */
   if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
