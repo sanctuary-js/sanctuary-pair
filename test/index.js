f25952353e018b46659d513c1bc0b56236d05ce3
David Chambers <dc@davidchambers.me>
2024-01-08 20:25:39 +0100

    use `assert.deepStrictEqual`

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
-import assert       from 'assert';
+import {deepStrictEqual as eq} from 'assert';
 
-import laws         from 'fantasy-laws';
-import jsc          from 'jsverify';
-import test         from 'oletus';
-import Identity     from 'sanctuary-identity';
-import show         from 'sanctuary-show';
-import Z            from 'sanctuary-type-classes';
-import type         from 'sanctuary-type-identifiers';
-import Useless      from 'sanctuary-useless';
+import laws from 'fantasy-laws';
+import jsc from 'jsverify';
+import test from 'oletus';
+import Identity from 'sanctuary-identity';
+import show from 'sanctuary-show';
+import Z from 'sanctuary-type-classes';
+import type from 'sanctuary-type-identifiers';
+import Useless from 'sanctuary-useless';
 
-import Pair         from '../index.js';
+import Pair from '../index.js';
 
 
 //    IdentityArb :: Arbitrary a -> Arbitrary (Identity a)
@@ -25,171 +25,161 @@ const PairArb = arbL => arbR => (jsc.pair (arbL, arbR)).smap (
 //    testLaws :: String -> Object -> Object -> Undefined
 const testLaws = typeClass => laws => arbs => {
   (Object.keys (laws)).forEach (name => {
-    eq (laws[name].length) (arbs[name].length);
+    eq (laws[name].length, arbs[name].length);
     const prettyName = name.replace (/[A-Z]/g, c => ' ' + c.toLowerCase ());
     test (`${typeClass} laws \x1B[2m›\x1B[0m ${prettyName}`,
           laws[name] (...arbs[name]));
   });
 };
 
-//    eq :: a -> b -> Undefined !
-function eq(actual) {
-  assert.strictEqual (arguments.length, eq.length);
-  return function eq$1(expected) {
-    assert.strictEqual (arguments.length, eq$1.length);
-    assert.strictEqual (show (actual), show (expected));
-    assert.strictEqual (Z.equals (actual, expected), true);
-  };
-}
-
 
 test ('metadata', () => {
-  eq (typeof Pair) ('function');
-  eq (Pair.name) ('Pair');
-  eq (Pair.length) (1);
+  eq (typeof Pair, 'function');
+  eq (Pair.name, 'Pair');
+  eq (Pair.length, 1);
 });
 
 test ('@@type', () => {
-  eq (type (Pair (0) (0))) ('sanctuary-pair/Pair@1');
-  eq (type.parse (type (Pair (0) (0))))
-     ({namespace: 'sanctuary-pair', name: 'Pair', version: 1});
+  eq (type (Pair (0) (0)), 'sanctuary-pair/Pair@1');
+  eq (type.parse (type (Pair (0) (0))),
+      {namespace: 'sanctuary-pair', name: 'Pair', version: 1});
 });
 
 test ('@@show', () => {
-  eq (show (Pair (['foo', 'bar', 'baz']) (-0)))
-     ('Pair (["foo", "bar", "baz"]) (-0)');
+  eq (show (Pair (['foo', 'bar', 'baz']) (-0)),
+      'Pair (["foo", "bar", "baz"]) (-0)');
 });
 
 test ('@@iterator', () => {
   const pair = Pair ('foo') ('bar');
   const iterator = pair[Symbol.iterator] ();
-  eq (iterator.next ()) ({value: 'foo', done: false});
-  eq (iterator.next ()) ({value: 'bar', done: false});
-  eq (iterator.next ()) ({value: undefined, done: true});
+  eq (iterator.next (), {value: 'foo', done: false});
+  eq (iterator.next (), {value: 'bar', done: false});
+  eq (iterator.next (), {value: undefined, done: true});
 });
 
 test ('Pair.pair', () => {
   eq (Pair.pair (a => b => 'Pair (' + show (a) + ') (' + show (b) + ')')
-                (Pair ('abc') ([1, 2, 3])))
-     ('Pair ("abc") ([1, 2, 3])');
+                (Pair ('abc') ([1, 2, 3])),
+      'Pair ("abc") ([1, 2, 3])');
 });
 
 test ('Pair.fst', () => {
-  eq (Pair.fst (Pair ('foo') (42))) ('foo');
+  eq (Pair.fst (Pair ('foo') (42)), 'foo');
 });
 
 test ('Pair.snd', () => {
-  eq (Pair.snd (Pair ('foo') (42))) (42);
+  eq (Pair.snd (Pair ('foo') (42)), 42);
 });
 
 test ('Pair.swap', () => {
-  eq (Pair.swap (Pair ('foo') (42))) (Pair (42) ('foo'));
+  eq (Pair.swap (Pair ('foo') (42)), Pair (42) ('foo'));
 });
 
 test ('Setoid', () => {
-  eq (Z.Setoid.test (Pair (Useless) (Useless))) (false);
-  eq (Z.Setoid.test (Pair (Useless) ('world'))) (false);
-  eq (Z.Setoid.test (Pair ('hello') (Useless))) (false);
-  eq (Z.Setoid.test (Pair ('hello') ('world'))) (true);
+  eq (Z.Setoid.test (Pair (Useless) (Useless)), false);
+  eq (Z.Setoid.test (Pair (Useless) ('world')), false);
+  eq (Z.Setoid.test (Pair ('hello') (Useless)), false);
+  eq (Z.Setoid.test (Pair ('hello') ('world')), true);
 });
 
 test ('Ord', () => {
-  eq (Z.Ord.test (Pair (Useless) (Useless))) (false);
-  eq (Z.Ord.test (Pair (Useless) ('world'))) (false);
-  eq (Z.Ord.test (Pair ('hello') (Useless))) (false);
-  eq (Z.Ord.test (Pair ('hello') ('world'))) (true);
+  eq (Z.Ord.test (Pair (Useless) (Useless)), false);
+  eq (Z.Ord.test (Pair (Useless) ('world')), false);
+  eq (Z.Ord.test (Pair ('hello') (Useless)), false);
+  eq (Z.Ord.test (Pair ('hello') ('world')), true);
 });
 
 test ('Semigroupoid', () => {
-  eq (Z.Semigroupoid.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Semigroupoid.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Category', () => {
-  eq (Z.Category.test (Pair (Math.sqrt) (Math.sqrt))) (false);
+  eq (Z.Category.test (Pair (Math.sqrt) (Math.sqrt)), false);
 });
 
 test ('Semigroup', () => {
-  eq (Z.Semigroup.test (Pair (Useless) (Useless))) (false);
-  eq (Z.Semigroup.test (Pair (Useless) ('world'))) (false);
-  eq (Z.Semigroup.test (Pair ('hello') (Useless))) (false);
-  eq (Z.Semigroup.test (Pair ('hello') ('world'))) (true);
+  eq (Z.Semigroup.test (Pair (Useless) (Useless)), false);
+  eq (Z.Semigroup.test (Pair (Useless) ('world')), false);
+  eq (Z.Semigroup.test (Pair ('hello') (Useless)), false);
+  eq (Z.Semigroup.test (Pair ('hello') ('world')), true);
 });
 
 test ('Monoid', () => {
-  eq (Z.Monoid.test (Pair ('hello') ('world'))) (false);
+  eq (Z.Monoid.test (Pair ('hello') ('world')), false);
 });
 
 test ('Functor', () => {
-  eq (Z.Functor.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Functor.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Bifunctor', () => {
-  eq (Z.Bifunctor.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Bifunctor.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Profunctor', () => {
-  eq (Z.Profunctor.test (Pair (Math.sqrt) (Math.sqrt))) (false);
+  eq (Z.Profunctor.test (Pair (Math.sqrt) (Math.sqrt)), false);
 });
 
 test ('Apply', () => {
-  eq (Z.Apply.test (Pair (Useless) (Useless))) (false);
-  eq (Z.Apply.test (Pair (Useless) ('world'))) (false);
-  eq (Z.Apply.test (Pair ('hello') (Useless))) (true);
-  eq (Z.Apply.test (Pair ('hello') ('world'))) (true);
+  eq (Z.Apply.test (Pair (Useless) (Useless)), false);
+  eq (Z.Apply.test (Pair (Useless) ('world')), false);
+  eq (Z.Apply.test (Pair ('hello') (Useless)), true);
+  eq (Z.Apply.test (Pair ('hello') ('world')), true);
 });
 
 test ('Applicative', () => {
-  eq (Z.Applicative.test (Pair ([]) ([]))) (false);
+  eq (Z.Applicative.test (Pair ([]) ([])), false);
 });
 
 test ('Chain', () => {
-  eq (Z.Chain.test (Pair (Useless) (Useless))) (false);
-  eq (Z.Chain.test (Pair (Useless) ('world'))) (false);
-  eq (Z.Chain.test (Pair ('hello') (Useless))) (true);
-  eq (Z.Chain.test (Pair ('hello') ('world'))) (true);
+  eq (Z.Chain.test (Pair (Useless) (Useless)), false);
+  eq (Z.Chain.test (Pair (Useless) ('world')), false);
+  eq (Z.Chain.test (Pair ('hello') (Useless)), true);
+  eq (Z.Chain.test (Pair ('hello') ('world')), true);
 });
 
 test ('ChainRec', () => {
-  eq (Z.ChainRec.test (Pair (Useless) (Useless))) (false);
-  eq (Z.ChainRec.test (Pair (Useless) ('world'))) (false);
-  eq (Z.ChainRec.test (Pair ('hello') (Useless))) (false);
-  eq (Z.ChainRec.test (Pair ('hello') ('world'))) (false);
+  eq (Z.ChainRec.test (Pair (Useless) (Useless)), false);
+  eq (Z.ChainRec.test (Pair (Useless) ('world')), false);
+  eq (Z.ChainRec.test (Pair ('hello') (Useless)), false);
+  eq (Z.ChainRec.test (Pair ('hello') ('world')), false);
 });
 
 test ('Monad', () => {
-  eq (Z.Monad.test (Pair ([]) ([]))) (false);
+  eq (Z.Monad.test (Pair ([]) ([])), false);
 });
 
 test ('Alt', () => {
-  eq (Z.Alt.test (Pair ([]) ([]))) (false);
+  eq (Z.Alt.test (Pair ([]) ([])), false);
 });
 
 test ('Plus', () => {
-  eq (Z.Plus.test (Pair ([]) ([]))) (false);
+  eq (Z.Plus.test (Pair ([]) ([])), false);
 });
 
 test ('Alternative', () => {
-  eq (Z.Alternative.test (Pair ([]) ([]))) (false);
+  eq (Z.Alternative.test (Pair ([]) ([])), false);
 });
 
 test ('Foldable', () => {
-  eq (Z.Foldable.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Foldable.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Traversable', () => {
-  eq (Z.Traversable.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Traversable.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Extend', () => {
-  eq (Z.Extend.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Extend.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Comonad', () => {
-  eq (Z.Comonad.test (Pair (Useless) (Useless))) (true);
+  eq (Z.Comonad.test (Pair (Useless) (Useless)), true);
 });
 
 test ('Contravariant', () => {
-  eq (Z.Contravariant.test (Pair (Math.sqrt) (Math.sqrt))) (false);
+  eq (Z.Contravariant.test (Pair (Math.sqrt) (Math.sqrt)), false);
 });
 
 testLaws ('Setoid') (laws.Setoid) ({
